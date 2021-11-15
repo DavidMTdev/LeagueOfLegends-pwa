f7eb99a72aa591a299f792a10162c6fcc001bab7
DavidMTdev <david.teixeira31@gmail.com>
2021-11-15 20:10:31 +0100

    install redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { useSelector } from 'react-redux'
 
 import Router from './router'
 import GlobalStyle from './styles/GlobalStyle'
-import { DarkTheme } from './themes'
+// import { DarkTheme } from './themes'
 
 function App() {
+  const theme = useSelector(state => state.theme.value)
+
   return (
-    <ThemeProvider theme={DarkTheme}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
     </ThemeProvider>
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,20 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { ContainerDivLogin, ContainerNavLink, FormLogin, InputSubmit } from '../styles/styledComponents';
+import React, { useEffect, useState } from 'react'
+import {
+  ContainerDivLogin,
+  ContainerNavLink,
+  FormLogin,
+  InputSubmit
+} from '../styles/styledComponents'
 
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-      <ContainerDivLogin>
-          <FormLogin>
-            <input value={username} onChange={(e) => setUsername(e.target.value)} name="username" ></input>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" ></input>
-            <InputSubmit type="submit" placeholder="Connexion"></InputSubmit>
-          </FormLogin>
-      </ContainerDivLogin>
-  );
+    <ContainerDivLogin>
+      <FormLogin>
+        <input
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          name='username'
+        ></input>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type='password'
+          name='password'
+        ></input>
+        <InputSubmit type='submit' placeholder='Connexion'></InputSubmit>
+      </FormLogin>
+    </ContainerDivLogin>
+  )
 }
-
 
 export default Login
