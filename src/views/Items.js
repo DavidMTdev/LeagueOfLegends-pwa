59292b230d3399a2d239f04d92fd76283b2ae929
DavidMTdev <david.teixeira31@gmail.com>
2021-11-19 12:54:15 +0100

    add system select item

--- src/views/Items.js
+++ src/views/Items.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import actions from '../actions'
@@ -10,14 +10,21 @@ const Items = () => {
   const loading = useSelector(state => state.items.loading)
   const error = useSelector(state => state.items.error)
   const items = useSelector(state => state.items.items)
+  const token = useSelector(state => state.login.data)
+  const [currentItem, setCurrentItem] = useState(null)
 
   useEffect(() => {
     dispatch(actions.items.fetchItems('11.22.1', 'fr_FR'))
   }, [])
 
+  const onClickSelectItem = item => {
+    setCurrentItem(item)
+  }
+
   const showItems = Object.keys(items).map(key => (
     <div key={key}>
       <img
+        onClick={() => onClickSelectItem(items[key])}
         src={`http://ddragon.leagueoflegends.com/cdn/11.22.1/img/item/${items[key].image.full}`}
       />
     </div>
@@ -26,7 +33,24 @@ const Items = () => {
   return (
     <div>
       {loading && <Loading size='1' />}
-      {error && <div>error</div>}
+      {error && <p>error</p>}
+
+      {currentItem && (
+        <div>
+          {token && <div>add</div>}
+          <p>{currentItem?.name}</p>
+          <p>{currentItem?.plaintext}</p>
+          {currentItem?.into?.map((key, index) => (
+            <div key={index}>
+              <p>{items[key].name}</p>
+              <img
+                onClick={() => onClickSelectItem(items[key])}
+                src={`http://ddragon.leagueoflegends.com/cdn/11.22.1/img/item/${items[key].image.full}`}
+              />
+            </div>
+          ))}
+        </div>
+      )}
       {items && <div>{showItems}</div>}
     </div>
   )
